a0691995ccd5223eff14bc0d8ebcdae8279e2741
xenleme <xenleme@gmail.com>
2018-04-21 18:15:24 +0300

    added two variants for writing fizzbuzz

---
+++ fizzbuzz/fizzbuzz1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func fizzbuzz(max int) {
+    for i := 1; i <= max; i++ {
+        switch {
+        case (i % 3 == 0) && (i % 5 == 0):
+            fmt.Println("FizzBuzz")
+        case i % 3 == 0:
+            fmt.Println("Fizz")
+        case i % 5 == 0:
+            fmt.Println("Buzz")
+        default:
+            fmt.Println(i)
+        }
+    }
+}
+
+func main() {
+    fizzbuzz(100)
+}
